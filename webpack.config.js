c310b4a9b808149db4844201e9fe8378562ff53e
shekeraoleksandr <oleksandrshekera@gmail.com>
2025-12-09 13:04:50 +0200

    Webpack 5 update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
 module.exports = {
+	mode: 'development',
+	entry: './src/client/index.js',
+	output: {
+		path: path.resolve(__dirname, 'static'),
+		filename: 'bundle.js'
+	},
 	module: {
 		rules: [
 			{
